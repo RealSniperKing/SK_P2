91516aa6f98bd272a8649af48ca605b96d0c8bcb
LN_SniperKing <luke.c@laposte.net>
2021-02-05 20:58:07 +0100

    merge_link_to_url + get_all_books_categories_urls

--- main_p2.py
+++ main_p2.py
@@ -43,9 +43,20 @@ def get_datas_product_from_url(url):
             print(str(tr.find("td").text))
             print("----------")
 
+def merge_link_to_url(url, link):
+    url_split = url.split("/")
+    value = count_directories_parents(link)
+
+    for i in range(0, value + 1):
+        del url_split[len(url_split) - 1]
+
+    path_OK = convert_array_to_string(url_split, '/') + "/" + link.replace("../", "")
+
+    return path_OK
+
 def get_all_books_categories_urls(url):
     response = requests.get(url)
-    titles_links = []
+    titles_and_links = []
     if response.ok:
         soup = BeautifulSoup(response.text, "html.parser")
         uls = soup.findAll("ul")
@@ -58,41 +69,30 @@ def get_all_books_categories_urls(url):
                     for a_content in a_contents:
                         link_title = a_content.text.strip()  # delete all ending spaces
                         link = a_content["href"]
-                        #print("link = " + str(link))
-                        value = count_directories_parents(link)
+                        new_url = merge_link_to_url(url, link)
+                        titles_and_links.append([link_title, new_url])
+    return titles_and_links
 
-                        #print("url = " + str(url))
-                        url_split = url.split("/")
-                        #print("url_split = " + str(url_split))
-                        for i in range(0, value + 1):
-                            del url_split[len(url_split) - 1]
-
-                        cleaned_path = convert_array_to_string(url_split, '/') + link.replace("..", "")
-                        #print("cleaned_path = " + str(cleaned_path))
-                        titles_links.append([link_title, cleaned_path])
-    return titles_links
-
-def get_all_books_items_urls(url):
+def get_all_books_items_urls(url, list_pages):
+    next_link = ""
     response = requests.get(url)
     if response.ok:
-        print("get_all_books_items_urls")
+        #print("get_all_books_items_urls")
         soup = BeautifulSoup(response.text, "html.parser")
         sec = soup.find("section")
 
-        # get link in current pages
-        ols = sec.findAll("ol")
-        print("ols = " + str(ols))
-
-
-        print("--------------------------------")
-
-        # check if exist others page
-
-
-        # a_contents = soup.findAll("a")
-        # for a_content in a_contents:
-        #     print("a_content = " + str(a_content))
-
+        a_contents = sec.findAll("a")
+        for a_content in a_contents:
+            link_title = a_content.text.strip()  # delete all ending spaces
+            link = a_content["href"]
+            if link_title != "":
+                new_url = merge_link_to_url(url, link)
+                #print(link_title + "    " + str(new_url))
+                list_pages.append(new_url)
+                if link_title == "next":
+                    next_link = new_url
+                    #print("next = " + str(new_url))
+    return list_pages, next_link
 # test = "../../../changing-the-game-play-by-play-2_317/index.html"
 # value = count_directories_parents(test)
 # print("value = " + str(value))
@@ -107,13 +107,17 @@ titles_urls = get_all_books_categories_urls(url_main)
 for title_url in titles_urls:
     title = title_url[0]
     url = title_url[1]
-    #print("url = " + str(url))
 
-    if title == "Romance":
-        print("test")
-        print("url = " + str(url))
-        get_all_books_items_urls(url)
+    if title != "Books":
+        #if title == "Romance":
+        books_pages = []
+        books_pages, next_page = get_all_books_items_urls(url, books_pages)
+        #print(title + "    " + str(len(books_pages)) + "    " + str(next_page))
 
+        while next_page != "":
+            books_pages, next_page = get_all_books_items_urls(next_page, books_pages)
+
+        print(title + "    " + str(len(books_pages)) + "    " + str(next_page))
 
 
 
