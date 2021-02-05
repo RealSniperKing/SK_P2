b65fe1592a7255661f2c4d19367a7fb76705906e
LN_SniperKing <luke.c@laposte.net>
2021-02-05 15:30:51 +0100

    add new list = titles_urls

--- main_p2.py
+++ main_p2.py
@@ -1,6 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+def convert_array_to_string(tableau, separator):
+    newLine = ''
+    for i, elem in enumerate(tableau, 0):
+        if i == 0:
+            newLine += str(elem)
+        else:
+            newLine += str(separator) + str(elem)
+    return newLine
+
 def get_datas_product_from_url(url):
     response = requests.get(url)
     if response.ok:
@@ -18,8 +27,9 @@ def get_datas_product_from_url(url):
             print(str(tr.find("td").text))
             print("----------")
 
-def get_all_books_categories_url(url):
+def get_all_books_categories_urls(url):
     response = requests.get(url)
+    titles_links = []
     if response.ok:
         soup = BeautifulSoup(response.text, "html.parser")
         uls = soup.findAll("ul")
@@ -28,16 +38,39 @@ def get_all_books_categories_url(url):
 
             if books_state != None:
                 if books_state.text == "Books":
-                    links = ul.findAll("a")
-                    print(links)
+                    a_contents = ul.findAll("a")
+                    for a_content in a_contents:
+                        link_title = a_content.text.strip()  # delete all ending spaces
+                        link = a_content["href"]
 
-            print("-------------")
+                        relative_infos = link.split("/")[0]
+                        if ".." in relative_infos:
+                            url_split = url.split("/")
 
-url_page = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+                            # delete last item for each '.'
+                            for i in relative_infos:
+                                del url_split[len(url_split)-1]
+
+                            cleaned_path = convert_array_to_string(url_split, '/') + link.replace("..", "")
+                            titles_links.append([link_title, cleaned_path])
+    return titles_links
+
+def get_all_books_items_urls(url):
+    print("get books")
+
 url_main = 'http://books.toscrape.com/catalogue/category/books_1/index.html'
+# Get urls categories from main page
+titles_urls = get_all_books_categories_urls(url_main)
 
-#get_datas_product_from_url
-get_all_books_categories_url(url_main)
+for title_url in titles_urls:
+    title = title_url[0]
+    url = title_url[1]
+
+    if title == "Romance":
+        get_all_books_items_urls(url)
+
+
+
 
 
 
