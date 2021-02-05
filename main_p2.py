dad530a3f192e5c5048d4c66e67bf5cbde8ac750
LN_SniperKing <luke.c@laposte.net>
2021-02-05 12:41:17 +0100

    First commit : get header url

---
+++ main_p2.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://books.toscrape.com'
+response = requests.get(url)
+
+if response.ok:
+    print("response = " + str(response))
+    print(response.headers)
